52efc264fd3b2acdb731be29c88c981b6ed76e38
Sokhavuth <vuthdevelop@gmail.com>
2023-07-20 12:30:41 +0700

    Make it better

--- index.js
+++ index.js
@@ -1,7 +1,15 @@
 import express from "express";
+import path from 'path'
 
 const app = express();
 const port = 3000;
+const __dirname = path.resolve()
+
+app.set('views', path.join(__dirname, 'views'))
+app.set('view engine', 'ejs')
+app.use(express.static(path.join(__dirname, 'public')))
+app.use(express.urlencoded({extended:false}))
+app.use(express.json())
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
